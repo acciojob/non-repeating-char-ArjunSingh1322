cd33a6d9f1d806b03dd2cd4dac0b5768573f521d
ArjunSingh1322 <as3388776@gmail.com>
2025-03-18 14:44:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
+
 function firstNonRepeatedChar(str) {
- // Write your code here
+ // Write your code 
+	let map = new Map();
+	for(let t of str){
+		if (map.has(t)){
+			let v = map.get(t)+1
+			map.set(t,v)
+		}
+		else{
+			map.set(t,1)
+		}
+	}
+		// 	console.log( map.values())
+for(let [key,value] of map){
+  if(value==1){
+    return key
+  }
+  else{
+    return null;
+  }
 }
+
+
+}
+console.log(firstNonRepeatedChar("aasshh"))
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
